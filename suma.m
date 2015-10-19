5792fbd13181f2a112a4aa339621b660574d7451
Ricardo Canales <canalesb93@gmail.com>
2015-10-19 00:05:45 -0500

    Adds function suma

---
+++ suma.m
@@ -0,0 +1,31 @@
+function prod_acum = suma(padre, prod)
+
+  N = length(padre);
+
+  A = zeros(N);
+  for i=1:N
+     if padre(i)~=0
+        A(i,padre(i)) = 1;
+     end
+  end
+
+  Aux = A;
+  prod_acum = zeros(size(padre));    % potencia que enfía cada nodo a su padre
+  while 1
+     hojas = find(sum(Aux,1)==0);  % si el nodo raíz es hoja,
+     if isequal(hojas,1)         % salir
+        break
+     end
+     prod_acum(hojas) = prod_acum(hojas) + prod(hojas);  
+     for i=1:length(hojas)
+        prod_acum(padre(hojas(i))) = prod_acum(padre(hojas(i))) + prod_acum(hojas(i));
+        Aux(hojas(i),:) = zeros(size(padre));
+     end
+     Aux(1,hojas) = 1;
+     %display(A);
+  end
+  display(prod_acum)
+
+
+
+endfunction
